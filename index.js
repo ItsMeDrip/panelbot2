53e82d896a338caa27fe62e5a89928c71c833d84
ItsMeDrip <darshsomani4@gmail.com>
2026-06-08 16:26:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ const client = new Client({
   ]
 })
 
-const PANEL_CHANNEL_ID = '1510954258118479944'
-const STATUS_CHANNEL_ID = '1510956160549781545'
+const PANEL_CHANNEL_ID = '1513495154151915530'
+const STATUS_CHANNEL_ID = '1513496686352142527'
 const MAX_SLOTS = 7
 
 const registrations = new Map()
